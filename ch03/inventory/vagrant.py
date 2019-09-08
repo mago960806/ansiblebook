caa462bf52f4ce71149196c3ae04cdb4cdbad49d
mago960806 <mago960806@hotmail.com>
2019-09-08 12:08:28 +0800

    第三章待学习

--- ch03/inventory/vagrant.py
+++ ch03/inventory/vagrant.py
@@ -1,54 +1,50 @@
 #!/usr/bin/env python
-# Adapted from Mark Mandel's implementation
-# https://github.com/ansible/ansible/blob/stable-2.1/contrib/inventory/vagrant.py
 
-import argparse
-import json
-import paramiko
-import subprocess
 import sys
+import json
+import argparse
+import subprocess
 
+import paramiko
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Vagrant inventory script")
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('--list', action='store_true')
-    group.add_argument('--host')
+    group.add_argument('--list', action='store_true', help="list vagrant hostnames")
+    group.add_argument('--host', help="show vagrant host details")
     return parser.parse_args()
-
 
 def list_running_hosts():
     cmd = "vagrant status --machine-readable"
-    status = subprocess.check_output(cmd.split()).rstrip()
+    status = subprocess.check_output(cmd.split()).decode().rstrip()
     hosts = []
     for line in status.split('\n'):
-        (_, host, key, value) = line.split(',')[:4]
+        _, host, key, value = line.split(',')[:4]
         if key == 'state' and value == 'running':
             hosts.append(host)
     return hosts
 
-
 def get_host_details(host):
-    cmd = "vagrant ssh-config {}".format(host)
+    cmd = f"vagrant ssh-config {host}"
     p = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
-    config = paramiko.SSHConfig()
-    config.parse(p.stdout)
-    c = config.lookup(host)
-    return {'ansible_host': c['hostname'],
-            'ansible_port': c['port'],
-            'ansible_user': c['user'],
-            'ansible_private_key_file': c['identityfile'][0]}
-
+    ssh = paramiko.SSHConfig()
+    ssh.parse(p.stdout)
+    config = ssh.lookup(host)
+    return {
+        'ansible_host': config['hostname'],
+        'ansible_port': config['port'],
+        'ansible_user': config['user'],
+        'ansible_private_key_file': config['identityfile'][0],
+    }
 
 def main():
     args = parse_args()
     if args.list:
         hosts = list_running_hosts()
-        json.dump({'vagrant': hosts}, sys.stdout)
+        details = {'vagrant': hosts}
     else:
         details = get_host_details(args.host)
-        json.dump(details, sys.stdout)
+    json.dump(details, sys.stdout)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
